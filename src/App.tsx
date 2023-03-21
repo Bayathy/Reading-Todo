900fb34d23f31019ed73f9316e4871a0051b0bdb
Bayathy <misosoup0321@icloud.com>
2023-03-22 00:52:46 +0900

    feat: update pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import type { FC } from 'react'
 
+import { Icon } from '@iconify/react'
+import * as RadixTabs from '@radix-ui/react-tabs'
+
+import { Card } from './components/Card'
+import { FormDialog } from './components/FormDialog'
+
 export const App: FC = () => {
   return (
-    <div>
-      <p className="w-16 text-3xl">Hello World</p>
-    </div>
+    <main className="m-auto h-full w-full max-w-4xl p-4">
+      <RadixTabs.Root
+        className="m-auto flex h-full w-full max-w-4xl flex-col"
+        defaultValue="tab1"
+      >
+        <RadixTabs.Content value="tab1">
+          <div className="flex justify-end">
+            <FormDialog />
+          </div>
+          <section className="mt-2">
+            <Card />
+          </section>
+        </RadixTabs.Content>
+        <RadixTabs.List className="fixed bottom-0 left-[50%] flex h-16 w-full translate-x-[-50%] border-t-2">
+          <RadixTabs.Trigger
+            value="tab1"
+            className="flex flex-1 items-center justify-center text-4xl hover:bg-slate-200"
+          >
+            <Icon icon="material-symbols:article-outline" />
+          </RadixTabs.Trigger>
+          <RadixTabs.Trigger
+            value="tab2"
+            className="flex flex-1 items-center justify-center text-4xl hover:bg-slate-200"
+          >
+            <Icon icon="material-symbols:history" />
+          </RadixTabs.Trigger>
+        </RadixTabs.List>
+      </RadixTabs.Root>
+    </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { App } from './App'
+import { Header } from './components/Header'
 import './style/global.css'
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <div className="grid min-h-screen w-full grid-cols-[100%] grid-rows-[auto_1fr]">
+      <Header />
+      <App />
+    </div>
   </React.StrictMode>,
 )
