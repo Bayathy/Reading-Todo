c13e2ba22bb39ba49be854ad034e22043ae728c7
Bayathy <misosoup0321@icloud.com>
2023-03-21 20:03:40 +0900

    feat: create Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+
+export const Header: FC = () => (
+  <header className="h-max border-b-2">
+    <div className="flex h-16 items-center">
+      <h1 className="m-auto text-center text-xl">記事一覧</h1>
+    </div>
+  </header>
+)
