e35b660a4005d878c3dd3b0f9d4c089156b314e2
Bayathy <misosoup0321@icloud.com>
2023-03-21 22:17:46 +0900

    feat: change header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
 import type { FC } from 'react'
 
+import { DropDownMenu } from '../DropDownMenu'
+
 export const Header: FC = () => (
   <header className="h-max border-b-2">
     <div className="flex h-16 items-center">
       <h1 className="m-auto text-center text-xl">記事一覧</h1>
+      <div className="absolute right-4">
+        <DropDownMenu />
+      </div>
     </div>
   </header>
 )
