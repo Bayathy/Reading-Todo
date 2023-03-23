e402ed53dfe9ba9fc35c9a3370c4af1f18f59a0a
Bayathy <misosoup0321@icloud.com>
2023-03-24 01:07:28 +0900

    feat; update page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { RecoilRoot } from 'recoil'
+
 import { App } from './App'
 import { Header } from './components/Header'
 import './style/global.css'
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
   <React.StrictMode>
-    <div className="grid min-h-screen w-full grid-cols-[100%] grid-rows-[auto_1fr]">
-      <Header />
-      <App />
-    </div>
+    <RecoilRoot>
+      <div className="grid min-h-screen w-full grid-cols-[100%] grid-rows-[auto_1fr]">
+        <Header />
+        <App />
+      </div>
+    </RecoilRoot>
   </React.StrictMode>,
 )
