68ab7c3bed548941ea5d51453f155c1c9bbd6fb2
Bayathy <misosoup0321@icloud.com>
2023-04-10 22:32:16 +0900

    feat: create Header

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,11 +1,14 @@
 import type { FC } from 'react'
 
-import type { HeaderProps } from '@/components/features/Header/header.types'
-
+export type HeaderProps = {
+  text: string
+}
 export const Header: FC<HeaderProps> = ({ text }) => {
   return (
-    <header className="h-16 bg-blue-500">
-      <div>{text}</div>
+    <header className="h-16">
+      <div className="m-auto flex h-full items-center px-4">
+        <h1 className="text-2xl">{text}</h1>
+      </div>
     </header>
   )
 }
