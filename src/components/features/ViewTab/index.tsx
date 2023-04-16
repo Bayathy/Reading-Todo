390389a919330f10b820c3768263c846af789854
Bayathy <misosoup0321@icloud.com>
2023-04-16 22:46:16 +0900

    feat: create ViewTab component

---
+++ src/components/features/ViewTab/index.tsx
@@ -0,0 +1,1 @@
+export { ViewTab } from './view-tab'
--- src/components/features/ViewTab/view-tab.tsx
+++ src/components/features/ViewTab/view-tab.tsx
@@ -1,11 +1,21 @@
 import type { FC } from 'react'
 
+import { InlineIcon } from '@iconify/react'
+
 export const ViewTab: FC = () => {
   return (
-    <nav>
-      <ul>
-        <li>未読</li>
-        <li>既読</li>
+    <nav className="fixed bottom-0 z-10 flex h-16 w-full items-center justify-center border-t-2 bg-white ">
+      <ul className="m-auto flex h-full w-full max-w-3xl items-center justify-around rounded-2xl ">
+        <li>
+          <button className="text-4xl">
+            <InlineIcon icon="material-symbols:article-rounded" />
+          </button>
+        </li>
+        <li>
+          <button className="text-4xl">
+            <InlineIcon icon="material-symbols:check-small-rounded" />
+          </button>
+        </li>
       </ul>
     </nav>
   )
