96ecd1b68621367ad29857c14175013d0816d2c8
Bayathy <misosoup0321@icloud.com>
2023-04-18 22:33:58 +0900

    feat: change home page

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,17 +1,85 @@
 import type { FC } from 'react'
+import { useEffect } from 'react'
 
-import { StoreProvider } from '@/components/providers/StoreProvider/StoreProvider'
-import { Header } from '@/components/ui'
+import { useSetRecoilState } from 'recoil'
+
+import { ViewArticleForm } from '@/components/features/ViewAricleForm/view-article-form'
+import { ViewArticleList } from '@/components/features/ViewArticleList'
+import { ViewHeader } from '@/components/features/ViewHeader'
+import { ViewTab } from '@/components/features/ViewTab'
+import { articleListStore } from '@/components/shared/store/article-store'
+import { Button, Modal } from '@/components/ui'
+import { useModal } from '@/components/ui/Modal/api/useModal'
 
 export const Home: FC = () => {
+  const setArticleList = useSetRecoilState(articleListStore)
+
+  useEffect(() => {
+    setArticleList([
+      {
+        id: 1,
+        title: 'Title1',
+        url: './',
+        done: false,
+        userId: 'uuid',
+        createdAt: new Date('2022-12-31T13:50:40+09:00'),
+      },
+      {
+        id: 2,
+        title: 'Title2',
+        url: './',
+        done: false,
+        userId: 'uuid',
+        createdAt: new Date('2023-01-01T13:50:40+09:00'),
+      },
+      {
+        id: 3,
+        title: 'Title3',
+        url: './',
+        done: false,
+        userId: 'uuid',
+        createdAt: new Date('2022-01-01T13:40:40+09:00'),
+      },
+      {
+        id: 4,
+        title: 'Title4',
+        url: './',
+        done: false,
+        userId: 'uuid',
+        createdAt: new Date('2021-12-31T13:50:40+09:00'),
+      },
+      {
+        id: 5,
+        title: 'Title4',
+        url: './',
+        done: false,
+        userId: 'uuid',
+        createdAt: new Date('2021-12-31T13:50:40+09:00'),
+      },
+    ])
+  }, [setArticleList])
+
+  const { ref, showModal, closeModal } = useModal()
+
   return (
-    <StoreProvider>
-      <div className="grid min-h-screen w-full grid-cols-[100%] grid-rows-[auto_1fr]">
-        <Header text={'Dash Board'} />
-        <main className="max-w-3xl px-4">
-          <p>Hello world</p>
-        </main>
-      </div>
-    </StoreProvider>
+    <div className="min-h-screen w-full">
+      <ViewHeader text={'Dash Board'} />
+      <main className="relative m-auto px-4">
+        <div className="mt-4 flex justify-end">
+          <Button onClick={showModal} color={'primary'}>
+            記事の追加
+          </Button>
+        </div>
+        <section className="mt-4">
+          <ViewArticleList />
+        </section>
+      </main>
+      <Modal onClose={closeModal} ref={ref}>
+        <ViewArticleForm />
+      </Modal>
+      <footer className={'mt-20'}>
+        <ViewTab />
+      </footer>
+    </div>
   )
 }
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,1 +1,1 @@
-export { Home } from './Home/Home'
+export { Home } from '@/components/pages/Home/Home'
