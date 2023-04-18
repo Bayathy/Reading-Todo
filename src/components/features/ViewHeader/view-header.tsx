b6fefaceae192d4b8b53910d8059104d0f27eb3d
Bayathy <misosoup0321@icloud.com>
2023-04-18 22:32:28 +0900

    feat: add login function at Header

---
+++ src/components/features/ViewHeader/view-header.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react'
+
+import { useAuth } from '@/components/shared/api/firebase/useAuth'
+import { Button } from '@/components/ui'
+
+export type HeaderProps = {
+  text: string
+}
+export const ViewHeader: FC<HeaderProps> = ({ text }) => {
+  const { authState, loginWithPopUp, logout } = useAuth()
+
+  return (
+    <header className="h-16 w-full border-b-2">
+      <div className="flex h-full items-center justify-between px-4">
+        <h1 className="text-2xl">{text}</h1>
+        <div>
+          {authState ? (
+            <Button color={'secondary'} onClick={() => logout()}>
+              ログアウト
+            </Button>
+          ) : (
+            <Button color={'secondary'} onClick={() => loginWithPopUp()}>
+              ログイン
+            </Button>
+          )}
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ src/components/shared/api/firebase/useAuth.ts
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react'
+
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth'
+
+import type { User } from 'firebase/auth'
+
+import { app } from '@/components/shared/api/firebase/init'
+
+export const useAuth = () => {
+  // eslint-disable-next-line unicorn/no-null
+  const [authState, setAuthState] = useState<User | null>(null)
+  const auth = getAuth(app)
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      setAuthState(user)
+    } else {
+      setAuthState(null)
+    }
+  })
+
+  const provider = new GoogleAuthProvider()
+  const loginWithPopUp = useCallback(
+    async () => await signInWithPopup(auth, provider),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  )
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const logout = useCallback(async () => await signOut(auth), [])
+
+  return { authState, loginWithPopUp, logout }
+}
