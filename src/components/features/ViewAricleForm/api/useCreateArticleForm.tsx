e051e4a6788ab8317c5cf6cdd02d1b0dc1da7d81
Bayathy <misosoup0321@icloud.com>
2023-04-29 18:04:01 +0900

    feat: add inspect plugin

--- src/components/features/ViewAricleForm/api/useCreateArticleForm.tsx
+++ src/components/features/ViewAricleForm/api/useCreateArticleForm.tsx
@@ -1,17 +1,21 @@
 import { useCallback } from 'react'
 
 import { useForm } from 'react-hook-form'
+import { useSetRecoilState } from 'recoil'
 import { useMutation } from 'urql'
+import * as z from 'zod'
 
 import type { NewArticle } from '@/gql/graphql'
 import type { SubmitHandler } from 'react-hook-form'
 
 import { createArticle } from '@/components/domains/Article/api/createArticle'
+import { articleListStore } from '@/components/shared/store/article-store'
 
-type ArticleFormState = {
-  url: string
-  title: string
-}
+const scheme = z.object({
+  url: z.string().min(1, { message: 'url is Required' }),
+})
+
+type ArticleFormState = z.infer<typeof scheme>
 
 export const useCreateArticleForm = () => {
   const {
@@ -20,15 +24,35 @@ export const useCreateArticleForm = () => {
     formState: { isValid, isDirty },
   } = useForm<ArticleFormState>({
     mode: 'onChange',
-    defaultValues: { title: '', url: '' },
+    defaultValues: { url: '' },
   })
+  const setArticleList = useSetRecoilState(articleListStore)
 
   const [newArticleResult, createNewArticle] = useMutation(createArticle)
   // eslint-disable-next-line unicorn/consistent-function-scoping
   const onArticleFormSubmit: SubmitHandler<ArticleFormState> = useCallback(
     async data => {
-      const variables: NewArticle = { userId: 'test', ...data }
-      await createNewArticle({ input: variables })
+      const variables: NewArticle = { userId: 'test', url: data.url }
+      const res = await createNewArticle({ input: variables })
+      try {
+        const newRecord = res.data?.createArticle
+        if (!newRecord) {
+          throw new Error('error')
+        }
+
+        setArticleList(oldList => [
+          ...oldList,
+          {
+            id: Number(newRecord.id),
+            url: newRecord.url,
+            userId: newRecord.userId,
+            createdAt: newRecord.createdAt,
+            done: newRecord.done,
+          },
+        ])
+      } catch (error: unknown) {
+        if (error instanceof Error) console.log(error.message)
+      }
     },
     [],
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), Inspect()],
   publicDir: './public',
   resolve: {
     alias: {
